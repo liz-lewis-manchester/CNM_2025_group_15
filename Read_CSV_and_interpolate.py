dfe9a0157d9751f2e4c24b51836247baa973e12d
sagebidwell <sage.bidwell@student.manchester.ac.uk>
2025-12-10 11:58:49 +0000

    Update Read_CSV_and_interpolate.py
    Updated the code to make more flexible and apply to all test cases, I will add the # later

--- Read_CSV_and_interpolate.py
+++ Read_CSV_and_interpolate.py
@@ -1,12 +1,22 @@
 import numpy as np 
 import pandas as pd 
 
-L=20 # length of the domain
-dx=0.2 # spatial interval 
-x_grid =np.arange(0, L + dx, dx) #create an array defining the start, stop and step
+def generate_initial_conditions(x_grid, mode='csv',file=None, pulse_location=None, pulse_strength=None):
+    #csv mode test case 2
+    if mode == 'csv':
+        if file is None:
+            raise ValueError("CSV mode requires file='path/to/file.csv'")
+        df = pd.read_csv(file)
+        x_data = df["x"].values
+        theta_data = df["concentration"].values
+        theta_initial = np.interp(x_grid, x_data, theta_data)
+        return theta_initial
+    #for single spike test case 1 
+    elif mode == "delta": 
+        if pulse_location is None or pulse_strength is None:
+            raise ValueError("Delta mode requires pulse_location and pulse_strength")
+        theta = np.zeros_like(x_grid)
+        idx = np.argmin(np.abs(x_grid - pulse_location))
+        theta[idx] = pulse_strength
+        return theta 
 
-df=pd.read_csv("initial_conditions.csv") #read CSV
-x_data = df["x"].values # extract data from CSV and returns a panda series which is then converted into a NumPy array
-
-theta_data = df["concentration"].values # extracts concentration data from CSV
-theta_initial= np.interp(x_grid, x_data, theta_data) # interpolate data onto grid producing a new NumPy array
