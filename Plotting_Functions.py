6077cb0fba359e55616dac61debf30634095c1a8
Hannahtw5 <hannahtw800@gmail.com>
2025-12-15 16:20:44 +0000

    Update Plotting_Functions.py
    adding additional content due to comments

--- Plotting_Functions.py
+++ Plotting_Functions.py
@@ -7,13 +7,15 @@ def function_snapshot (x, theta_history, t_grid, snapshot_times, save_dir="resul
   #t_grid = time values selected
   #snapshot_times = the paticular times plotted
   #save_dir = folder results is saved in (results)
+
+  colors = plt.cm.plasma(np.linspace(0, 1, len(snapshot_times))) #introducing colours to be used in plots
   
-  for t_target in snapshot_times: #loop time values that will be plotted
+  for i, t_target in enumerate(snapshot_times): #loop time values that will be plotted
     
     index = np.abs(t_grid - t_target).argmin()  #finding closest target time index in time grid
     
     plt.figure(figsize=(9,5))  
-    plt.plot(x, theta_history[index], linewidth=2.5) #plotting concentration vs distance
+    plt.plot(x, theta_history[index, :], color=colors[i], linewidth=2.5) #plotting concentration vs distance
     plt.title(fr"Pollutant concentration at $t = {t_grid[index]:.1f}\,\mathrm{{s}}$", fontsize=14, weight="bold")  #fr to accomodate LaTeX and and formatted text
     plt.xlabel(r"Distance downstream $x\,(\mathrm{m})$", fontsize=12)  #r for avoiding escaping issues
     plt.ylabel(r"Concentration $\theta\,(\mathrm{\mu g\,m^{-3}})$", fontsize=12)
